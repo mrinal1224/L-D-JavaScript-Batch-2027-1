9729f8f63960d71a80cfa5277924d5ac66c0e60e
mrinal1224 <iammrinalkc@gmail.com>
2026-06-03 16:08:23 +0530

    map polyfill

---
+++ Class-2 (HOF Polyfills)/index.js
@@ -0,0 +1,6 @@
+// Polyfills
+
+// Starting in 5 mins
+/// Map , Filter , Reduce
+
+
---
+++ Class-2 (HOF Polyfills)/map.js
@@ -0,0 +1,47 @@
+// Map
+// Map works with or on an Array
+// Prototype of an Array
+// internally it is running a loop
+// Data is returned in a New Array
+
+// let sqr = arr.map(function (n) {
+//   return n * n;
+// });
+
+// console.log(sqr)
+
+Array.prototype.myMap = function (callback) {
+  // Edge cases
+  if (typeof callback != "function") {
+    throw new Error("callback is not a function");
+  }
+
+  let resultArray = [];
+
+  for (let i = 0; i < this.length; i++) {
+    resultArray.push(callback(this[i] , this));
+  }
+
+  return resultArray;
+};
+
+let arr = [1, 2, 3, 4];
+
+let sqaures = arr.myMap(function (n) {
+  return 2 * n;
+});
+
+console.log(sqaures)
+
+
+
+const obj = {
+    multiplier: 2,
+    multiply(arr) {
+      return arr.map(function(num) {
+        return num * this.multiplier; // ❌ this is undefined
+      } , this);
+    }
+  };
+  
+  console.log(obj.multiply([1, 2, 3]));
